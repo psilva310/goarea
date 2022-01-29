12e993e98ac0529e8e99f2e61f84e1a72741336b
psilva310 <64285499+psilva310@users.noreply.github.com>
2022-01-29 16:17:37 -0300

    Criacao do arquivo

---
+++ main.go
@@ -0,0 +1,18 @@
+package goarea
+
+import "math"
+
+const Pi = 3.1416
+
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+//Não é visível
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
